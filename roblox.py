54da9237d34620b2823ee7260d237a79d1a9a5ad
Roblox123-beep <berbebt433@gmail.com>
2023-10-16 10:14:15 +0300

    smm

--- roblox.py
+++ roblox.py
@@ -1,9 +1,3 @@
 for i in range(15,90):
-    if i%5 == 0:
+    if i%5 == 0 and i%3 == 0 and i%2 == 0:
         print(i)
-    elif i%3 == 0:
-        print(i)
-    elif i%2 == 0:
-        print(i)
-    else:
-        print(" ")
